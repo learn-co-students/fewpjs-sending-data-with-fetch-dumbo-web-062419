efe1c2b74dd0ec0a105d1f1d757be6c974c28b14
Your Name <you@example.com>
2019-08-10 18:05:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData(name, email) {
+
+
+
+    return  fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name,
+            email
+        })
+    })
+        .then(response => response.json())
+        .then(slapOnDom)
+
+        function slapOnDom(json) {
+        document.body.innerHTML = json[ "id" ]
+        }
+
+}
